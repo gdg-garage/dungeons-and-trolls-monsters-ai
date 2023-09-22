2d937a5019a7566b391142a5067e82ae6c9e1b0d
Šimon Let <simon@betterstack.com>
2023-09-22 23:06:20 +0200

    Send logs to Better Stack

---
+++ stdin2betterstack.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Usage: go run main.go BOT_TOKEN | ./stdin2betterstack.sh BETTER_STACK_TOKEN
+# check if the token is passed
+if [ -z "$1" ]; then
+  echo "Please provide Better Stack token."
+  echo "USAGE: go run main.go BOT_TOKEN | ./stdin2betterstack.sh BETTER_STACK_TOKEN" 
+  exit 1
+fi
+
+while IFS=$'\n' read -r line; do
+  printf '%s\n' "$line"
+  curl -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer '"$1" \
+  -d '{"dt":"'"$(date -u +'%Y-%m-%d %T UTC')"'","message":"'"${line}"'"}' \
+  -k \
+  https://in.logs.betterstack.com
+done
