b4a3e7ca14bcc12ed187cf1b76315a703b4695ed
Šimon Let <simon@betterstack.com>
2023-09-23 21:15:12 +0200

    fix2

--- main.go
+++ main.go
@@ -14,32 +14,12 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+func fallbackLog(msg string) {
+	noPrefixLog := log.New(os.Stdout, "", 0)
+	noPrefixLog.Printf("{'level':'fatal', 'message': '%s'}\n", msg)
+}
+
 func main() {
-	stop, found := os.LookupEnv("DNT_PAUSE_APP")
-	if found && stop != "" {
-		log.Println("{ 'message': '>>> MONSTER AI IS PAUSED! Unset DNT_PAUSE_APP env variable to unpause. <<<' }")
-		os.Exit(0)
-	}
-	// Read command line arguments OR environment variables
-	apiKey, found := os.LookupEnv("DNT_API_KEY")
-	if !found {
-		if len(os.Args) < 2 {
-			log.Fatal("USAGE: ./dungeons-and-trolls-monsters-ai API_KEY")
-		} else {
-			apiKey = os.Args[1]
-		}
-	}
-
-	// Initialize the HTTP client and set the base URL for the API
-	cfg := swagger.NewConfiguration()
-	cfg.BasePath = "https://docker.tivvit.cz"
-
-	// Set the X-API-key header value
-	ctx := context.WithValue(context.Background(), swagger.ContextAPIKey, swagger.APIKey{Key: apiKey})
-
-	// Create a new client instance
-	client := swagger.NewAPIClient(cfg)
-
 	loggerConfig := zap.NewProductionConfig()
 
 	// Set key names and time format for Better Stack
@@ -56,9 +36,36 @@ func main() {
 	// Create the logger
 	logger, err := loggerConfig.Build()
 	if err != nil {
-		log.Fatalf("FATAL: Can't initialize zap logger: %v", err)
+		fallbackLog(fmt.Sprintf("FATAL: Can't initialize zap logger: %v", err))
+		os.Exit(1)
 	}
 	defer logger.Sync()
+
+	stop, found := os.LookupEnv("DNT_PAUSE_APP")
+	if found && stop != "" {
+		logger.Error("MONSTER AI IS PAUSED! Unset DNT_PAUSE_APP env variable to unpause.")
+		os.Exit(0)
+	}
+
+	// Read command line arguments OR environment variables
+	apiKey, found := os.LookupEnv("DNT_API_KEY")
+	if !found {
+		if len(os.Args) < 2 {
+			logger.Fatal("USAGE: ./dungeons-and-trolls-monsters-ai API_KEY")
+		} else {
+			apiKey = os.Args[1]
+		}
+	}
+
+	// Initialize the HTTP client and set the base URL for the API
+	cfg := swagger.NewConfiguration()
+	cfg.BasePath = "https://docker.tivvit.cz"
+
+	// Set the X-API-key header value
+	ctx := context.WithValue(context.Background(), swagger.ContextAPIKey, swagger.APIKey{Key: apiKey})
+
+	// Create a new client instance
+	client := swagger.NewAPIClient(cfg)
 
 	if len(os.Args) > 2 && os.Args[2] == "respawn" {
 		respawn(ctx, logger.Sugar(), client)
